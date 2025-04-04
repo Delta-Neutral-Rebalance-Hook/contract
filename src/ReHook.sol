0f9c9ac6a3526d598ace8ee0b0a09ee5f9695acf
b11901194 <b11901194@ntu.edu.tw>
2025-04-04 23:19:24 +0800

    create ReHook.sol & ReHook.t.sol

---
+++ src/ReHook.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {CurrencySettler} from "v4-core/test/utils/CurrencySettler.sol";
+import {BaseTestHooks} from "v4-core/src/test/BaseTestHooks.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+
+
+contract ReHook is BaseTestHooks {
+
+    using Hooks for IHooks;
+    using CurrencySettler for Currency;
+
+    IPoolManager immutable manager;
+
+    constructor(IPoolManager _manager) {
+        manager = _manager;
+    }
+
+    modifier onlyPoolManager() {
+        require(msg.sender == address(manager));
+        _;
+    }
+    function beforeSwap(
+        address, /* sender **/
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        bytes calldata /* hookData **/
+    ) external override onlyPoolManager returns (bytes4, BeforeSwapDelta, uint24) {
+        
+    }
+}
---
+++ test/ReHook.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {Test} from "forge-std/Test.sol";
+import {Deployers} from "v4-core/test/utils/Deployers.sol";
+import {IHooks} from "v4-core/src/interfaces/IHooks.sol";
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {PoolSwapTest} from "v4-core/src/test/PoolSwapTest.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {Currency} from "v4-core/src/types/Currency.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {SafeCast} from "v4-core/src/libraries/SafeCast.sol";
+import {Constants} from "v4-core/test/utils/Constants.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+import {BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+import {ReHook} from "../src/ReHook.sol";
+import {console2} from "forge-std/console2.sol";
+
+contract ReHookTest is Test, Deployers {
+
+    using SafeCast for *;
+
+    address hook;
+    address user = address(0xBEEF);
+
+    function setUp() public {
+        initializeManagerRoutersAndPoolsWithLiq(IHooks(address(0)));
+    }
+
+    function testRehook() public {
+
+
+        address impl = address(new ReHook(manager));
+        address hookAddr = address(uint160(Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_SWAP_RETURNS_DELTA_FLAG));
+        _etchHookAndInitPool(hookAddr, impl);
+        console2.log("hookAddr", hookAddr);
+
+    }
+    function _etchHookAndInitPool(address hookAddr, address implAddr) internal {
+        vm.etch(hookAddr, implAddr.code);
+        hook = hookAddr;
+        (key,) = initPoolAndAddLiquidity(currency0, currency1, IHooks(hook), 100, SQRT_PRICE_1_1);
+    }
+    function _defaultTestSettings() internal returns (PoolSwapTest.TestSettings memory testSetting) {
+        return PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
+    }
+
+}
